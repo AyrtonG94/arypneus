c26ee1797a8e9e13d879c682214e38f699225337
AyrtonG94 <ayrton.gomes@live.com>
2023-11-08 20:51:17 -0300

    footer

--- src/App.vue
+++ src/App.vue
@@ -23,7 +23,28 @@ import PneuComponent from './components/PneuComponent.vue';
     <PneuComponent></PneuComponent>
   </section>
  
-
+  <footer>
+    <div class="footer-container">
+      <div class="footer-section">
+        <h2>Sobre</h2>
+        <p>Referência de preço e qualidade em todo o Brasil.</p>
+      </div>
+      <div class="footer-section">
+        <h2>Links Úteis</h2>
+        <ul>
+          <li><a href="#">Página Inicial</a></li>
+          <li><a href="#">Produtos</a></li>
+          <li><a href="#">Contato</a></li>
+        </ul>
+      </div>
+      <div class="footer-section">
+        <h2>Contato</h2>
+        <p>Endereço:  Jardim Portugalia, Suzano - SP, 08613-135</p>
+        <p>Email: contato@arypneus.com</p>
+        <p>Telefone: (11) 3924-5570</p>
+      </div>
+    </div>
+  </footer>
 </template>
 
 <style scoped>
@@ -50,6 +71,57 @@ body {
 
 .pneu {
   margin-top: 80px;
+}
+
+
+/* Estilos para o footer */
+footer {
+  background-color: #333;
+  color: #fff;
+  padding: 20px 0;
+}
+
+.footer-container {
+  display: flex;
+  justify-content: space-around;
+  flex-wrap: wrap;
+}
+
+.footer-section {
+  flex: 1;
+  text-align: center;
+  margin: 0 20px;
+}
+
+.footer-section h2 {
+  font-size: 18px;
+  margin-bottom: 10px;
+}
+
+ul {
+  list-style: none;
+  padding: 0;
+}
+
+ul li {
+  margin-bottom: 5px;
+}
+
+a {
+  color: #fff;
+  text-decoration: none;
+}
+
+/* Estilos responsivos */
+@media (max-width: 768px) {
+  .footer-container {
+    flex-direction: column;
+  }
+
+  .footer-section {
+    flex: none;
+    margin: 10px 0;
+  }
 }
 
 
